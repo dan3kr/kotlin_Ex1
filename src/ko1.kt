6761072e901f4ceeef0cdeb4bf3d9b0f93b1e92a
young <jimjim4@outlook.kr>
2018-12-26 14:28:08 +0900

    이런기능까지~~

--- src/ko1.kt
+++ src/ko1.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>){
     val persons = listOf(Person("똥팔이", 30), Person("3D", age = 29))
     val oldest = persons.maxBy { it.age ?: 0 }
     println("웃기고 있네...썩글 : $oldest")
+    println("올 ~~ 이런 기능도 있고 좋네~~");
 
 println("")
 }
