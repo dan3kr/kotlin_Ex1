8949cc3eb1d616e99b8b2b644636802c79a40fd7
young <jimjim4@outlook.kr>
2018-12-26 14:19:22 +0900

    Revert "Revert "kotlin ex 2""
    This reverts commit 00138022a5a2f3707ca0812bf2619ef2e5fd2cc8.

---
+++ src/ko1.kt
@@ -0,0 +1,9 @@
+data class Person(val name: String, val age: Int ? = null)
+
+fun main(args: Array<String>){
+    val persons = listOf(Person("똥팔이", 30), Person("3D", age = 29))
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("나이가 가장 많은 사람 : $oldest")
+
+println("")
+}
